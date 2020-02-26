82bfa8ff1c246b999659dde5d2e6a0b1aaf82ecf
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-02-26 13:16:21 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using DepemdencyInjection;
+
+namespace UnitTestDI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockobj = new Mock<IDataAccess>();
+            mockobj.Setup(x => x.GetData(1)).Returns("Hello1");
+
+            var mockobj1 = new BuisnessLogic(mockobj.Object);
+            var data = mockobj1.GetData(1);
+            Assert.AreEqual(data, "Hello1");
+
+
+        }
+    }
+}
